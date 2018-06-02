8f130544a61e5620a23af249b8373c2facb90735
zimirken42 <eliseu42lucena@gmail.com>
2018-06-02 11:09:22 -0300

    operacoes matematicas

---
+++ aulas Zero/operacoesMatematica.py
@@ -0,0 +1,39 @@
+# operacoes matematica
+x = 53
+y = 42
+
+# operacoes comuns
+
+soma = x + y
+mult =  x * y
+div = x / y
+sub =  x - y
+
+# divisao inteira
+
+div_int = x // y
+
+# resto de Divisao
+
+rest_div = x % y
+
+# potencia
+
+potencia = x ** y
+
+print (soma)
+print (mult)
+print (div)
+print (sub)
+
+print ("Divisao inteira")
+
+print (div_int)
+
+print ("resto divisao")
+
+print (rest_div)
+
+print ("potencia")
+
+print (potencia)
